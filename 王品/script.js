b5c575d4fe3b4f02936489891a631e9031d52bb1
goodgod99 <107896456+goodgod99@users.noreply.github.com>
2023-10-24 03:09:36 -0500

    v4

--- 王品/script.js
+++ 王品/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
 
     // Initialize Swiper
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".mySwiper1", {
+        effect: "fade",
+        autoplay: {
+            delay: 5000, 
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        loop: true,
+    });
+    var swiper = new Swiper(".mySwiper2", {
+        effect: "fade",
+        autoplay: {
+            delay: 5000, 
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        loop: true,
+    });
+    var swiper = new Swiper(".mySwiper3", {
         effect: "fade",
         autoplay: {
             delay: 5000, 
@@ -13,11 +35,12 @@ $(document).ready(function () {
         loop: true,
     });
 
+
+
     // Initialize AOS
     $(function () {
         AOS.init({
             offset: 200,
-            once: true,
             duration: 700,
             delay: 100,
             easing: 'ease-in-sine',
