790c51d25c84cc3321ebc2d70aaf3c40e8924f86
goodgod99 <107896456+goodgod99@users.noreply.github.com>
2024-04-10 03:13:45 +0800

    js檔案分類

--- Front_End_Portfolio/js/nav_control.js
+++ Front_End_Portfolio/js/nav_control.js
@@ -1,31 +1,57 @@
 const buttons = document.querySelectorAll('nav ul li button');
-const projects = document.querySelectorAll('#project a');
+const menuControl = document.getElementById('menu-control');
+const menuLabel = document.getElementById('menu-label');
 
+//nav開關icon切換
+menuControl.addEventListener('change', () => {
+    menuLabel.innerText = menuControl.checked ? '✖' : '☰';
+});
+
+//點擊nav內button觸發
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        //變更nav button的樣式
+        //變更button的樣式
         buttons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
 
-        //顯示對應的project
+        //關閉menu
+        menuControl.checked = false;
+        menuLabel.innerText = menuControl.checked ? '✖' : '☰';
+
+        //滑動至頂端
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+        //顯示或隱藏對應的project
         const btnClass = button.id;
-        // if (btnClass === 'All') {
-        //     projects.forEach(project => {
-        //         project.style.display = 'block';
-        //     });
-        // }
-        // else{
-        //     projects.forEach(project => {
-        //         if (project.classList.contains(btnClass)) {
-        //             project.style.display = 'block';
-        //         } else {
-        //             project.style.display = 'none';
-        //         }
-        //     });
-        // }
         filterProjectsByCategory(btnClass);
-        
     });
 });
 
+// 根據點擊button過濾專案
+function filterProjectsByCategory(category) {
+    const projectContainer = document.querySelectorAll('#project a');
+    projectContainer.forEach(project => {
+        project.style.opacity = '0';
+        setTimeout(() => {
+            if (category === 'All') {
+                project.style.display = 'block';
+                setTimeout(() => {
+                    project.style.opacity = '1';
+                }, 100);
+            } else {
+                if (project.classList.contains(category)) {
+                    project.style.display = 'block';
+                    setTimeout(() => {
+                        project.style.opacity = '1';
+                    }, 100);
+                } else {
+                    project.style.display = 'none';
+                }
+            }
+        }, 500);
+    });
+}
 
