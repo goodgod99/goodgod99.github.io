13c879a9f92ff9aa7a8ec30f0b2068baf85ebc83
goodgod99 <107896456+goodgod99@users.noreply.github.com>
2024-04-10 14:20:14 +0800

    統一主視覺

--- Front_End_Portfolio/js/parallax_scrolling.js
+++ Front_End_Portfolio/js/parallax_scrolling.js
@@ -11,12 +11,15 @@ window.addEventListener('scroll', function () {
     let offset = window.pageYOffset;
     let wave = document.getElementById('wave-backgroung');
 
-    let speedMultiplier = -1.3;
-    if (window.innerWidth < 1200){
-        speedMultiplier = -0.5; 
-    }
+    let speedMultiplier;
     if (window.innerWidth < 576) { 
         speedMultiplier = -0.1; 
+    }
+    else if (576 <= window.innerWidth && window.innerWidth < 1200) {
+        speedMultiplier = -0.3; 
+    }
+    else{
+        speedMultiplier = -0.6;
     }
 
     wave.style.transform = 'translateY(' + offset * speedMultiplier + 'px)';
